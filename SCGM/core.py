0c866830820b9c0d6d430bd2ae81dee746bf146b
Joshua Shorenstein <jshorens@gmail.com>
2013-03-28 11:49:16 -0700

    Comment ALL THE THINGS

--- SCGM/core.py
+++ SCGM/core.py
@@ -11,19 +11,31 @@ __status__ = "Development"
 
 from SCGM.profile import make_profile_from_mapping, normalize_profiles
 
+
 def get_profiles_list(map_files, categories=None):
-	if not categories:
-		categories = ['HOST_SUBJECT_ID' for x in map_files]
+    """Creates a list of profiles for all map files, split by categories 
 
-	profiles = []
+    map_files: list of filepaths to mapping files
+    categories: list of categories, one per map_file, same order as map_files
+                allows comparison of metadata with different names but same data
+                Default: HOST_SUBJECT_ID
+    """
 
-	for map_file, category in zip(map_files, categories):
-		result = make_profile_from_mapping(map_file, category)
-		for key in result:
-			profiles.append(result[key])
+    #create default categories list if none passed
+    if not categories:
+        categories = ['HOST_SUBJECT_ID' for x in map_files]
 
-	return normalize_profiles(profiles)
+    profiles = []
+    #create a profile list for each mapping file, uisng category passed
+    for map_file, category in zip(map_files, categories):
+        result = make_profile_from_mapping(map_file, category)
+        #append each resulting profile to final profile list
+        for key in result:
+            profiles.append(result[key])
+    #normalize the profiles list before returning it
+    return normalize_profiles(profiles)
 
 
 def make_core_profile(map_files, categories=None):
-	return get_profiles_list(map_files, categories)
+    """ Wrapper function in case other calls are needed """
+    return get_profiles_list(map_files, categories)
--- SCGM/profile.py
+++ SCGM/profile.py
@@ -1,12 +1,26 @@
+#!/usr/bin/env python
+
+__author__ = "Jose Antonio Navas Molina"
+__copyright__ = "Copyright 2013, SCGM course project"
+__credits__ = ["Jose Antonio Navas Molina", "Joshua Shorenstein", "Elizabeth Lor"]
+__license__ = "GPL"
+__version__ = "0.0.1-dev"
+__maintainer__ = "Jose Antonio Navas Molina"
+__email__ = "josenavasmolina@gmail.com"
+__status__ = "Development"
+
 from qiime.parse import parse_mapping_file_to_dict
 from qiime.filter import sample_ids_from_metadata_description
 
 def normalize_profiles(profiles):
-    """Make sure all profiles contain the same taxa keys"""
+    """Make sure all profiles contain the same taxa keys
+
+        profiles: list of sample profiles
+    """
     if len(profiles) == 0:
         raise ValueError, "An empty profiles list cannot be normalized"
     if len(profiles) == 1:
-        raise ValueError, "More than one profile is needed to normalized"
+        raise ValueError, "More than one profile is needed to normalize"
     #Make a set of taxa keys for each profiles
     taxa = set([key for prof in profiles for key in prof.keys()])
     if 'not_shared' not in taxa:
@@ -20,7 +34,10 @@ def normalize_profiles(profiles):
     return profiles
 
 def compare_profiles(profiles):
-    """Compare the keys for profile 1 and profile 2 and take the minimum value"""
+    """Compare the keys over all profiles and take the minimum value
+
+        profiles: list of sample profiles
+    """
     if len(profiles) == 0:
         raise ValueError, "Cannot compare an empty list"
     if len(profiles) == 1:
@@ -29,42 +46,65 @@ def compare_profiles(profiles):
     share = 0.0
     #Start with the first taxa key of the profiles
     for key in profiles[0]:
+        #Skip the not_shared taxa, since is artificially added
         if key == "not_shared":
             continue
         #Find the minimum for the given taxa unit in the profiles
         result[key] = min([prof[key] for prof in profiles])
         share += result[key]
+    #Add the not_shared unit as remaining precentage to sum to 100%
     result['not_shared'] = 1.0 - share
     return result
 
 def make_profile(map_data, sids):
+    """ Create a comparison profile for the sampe IDs passed 
+
+        map_data: mapping file data from parse_mapping_file_to_dict()
+        sids: list of sample IDs to compare
+    """
     profiles = []
+    #create a profile dictionary for each sample ID passed
     for sid in sids:
         profile = {}
+        #loop over the keys in the mapping file
         for key in map_data[sid]:
+            #all keys with taxa data start with k_
             if key.startswith('k_'):
                 profile[key] = float(map_data[sid][key])
+        #append the profile to the profiles list
         profiles.append(profile)
 
+    #if we have only one profile, add not_shared and return it
     if len(profiles) == 1:
         profiles[0]['not_shared'] = 0.0
         return profiles[0]
-
+    #return the single comparison profile for all sample IDs passed
     return compare_profiles(normalize_profiles(profiles))
 
 def make_profile_from_mapping(mapping_fp, category="HOST_SUBJECT_ID"):
+    """ Create a list of comparison profiles for each unique value in category
+
+        mapping_fp: filepath to the mapping file of interest
+        category: mapping file category to split data over
+                  defaults to HOST_SUBJECT_ID
+    """
+    #parse the mapping file using qiime function
     map_f = open(mapping_fp, 'U')
     mapping_data, comments = parse_mapping_file_to_dict(map_f)
     map_f.close()
 
+    #get a list of unique keys for the specifified category
     values = set([mapping_data[sid][category] for sid in mapping_data])
 
     result = {}
+    #loop over each key found
     for value in values:
         map_f = open(mapping_fp, 'U')
+        #get sample ids that match the key using qiime function
         sids = sample_ids_from_metadata_description(map_f, category+":"+value)
         map_f.close()
 
+        #create the comarison profile for the sample IDs and add to result list
         result[value] = make_profile(mapping_data, sids)
 
     return result
