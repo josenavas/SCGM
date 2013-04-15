a5bd1a9613c0fd6233dc496c079827b10d38f15e
jose <jose@jose-K53SJ.(none)>
2013-04-15 12:06:39 -0600

    Renaming helper.py to util.py and fixing little issues with test_profile.py

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -11,17 +11,22 @@ __status__ = "Development"
 
 
 from cogent.util.unit_test import TestCase, main
-from SCGM.profile import compare_profiles, normalize_profiles, make_profile, make_profile_from_mapping
-from StringIO import StringIO
-
+from os.path import dirname, join
+from SCGM.profile import (compare_profiles, normalize_profiles, make_profile, 
+                            make_profile_from_mapping)
 
 class ProfileTests(TestCase):
     def setUp(self):
-        self.not_shared_profiles = [{'taxa1': 0.20, 'taxa2': 0.30, 'taxa3': 0.15, 'taxa4': 0.35, 'not_shared': 0.00},
+        self.not_shared_profiles = [
+            {'taxa1': 0.20, 'taxa2': 0.30, 'taxa3': 0.15, 'taxa4': 0.35,
+             'not_shared': 0.00},
             {'taxa1': 0.10, 'taxa2': 0.50, 'taxa3': 0.15, 'taxa4': 0.25}]
-        self.uneven_profiles = [{'taxa1': 0.20, 'taxa3': 0.30, 'taxa5': 0.15, 'taxa6': 0.35},
-            {'taxa1': 0.10, 'taxa2': 0.50, 'taxa4': 0.15, 'taxa5': 0.05, 'taxa7': 0.20}]
-        self.many_profiles = [{'taxa1': 0.20, 'taxa2': 0.30, 'taxa3': 0.15, 'taxa4': 0.35},
+        self.uneven_profiles = [
+            {'taxa1': 0.20, 'taxa3': 0.30, 'taxa5': 0.15, 'taxa6': 0.35},
+            {'taxa1': 0.10, 'taxa2': 0.50, 'taxa4': 0.15, 'taxa5': 0.05,
+             'taxa7': 0.20}]
+        self.many_profiles = [
+            {'taxa1': 0.20, 'taxa2': 0.30, 'taxa3': 0.15, 'taxa4': 0.35},
             {'taxa1': 0.10, 'taxa2': 0.50, 'taxa3': 0.15, 'taxa4': 0.25},
             {'taxa1': 0.15, 'taxa2': 0.22, 'taxa3': 0.15, 'taxa4': 0.48}]
         self.map_data = {
@@ -40,11 +45,12 @@ class ProfileTests(TestCase):
                     'k_kingdom1': '0.3',
                     'k_kingdom2': '0.2',
                     'k_kingdom3': '0.5',}}
-        self.mapping_fp = "./support_files/test_mapping.txt"
+        self.mapping_fp = join(dirname(__file__),
+            "./support_files/test_mapping.txt")
 
     def test_compare_profiles(self):
-        ''' Comparing three profiles, testing when each are the min and when one \
-        taxa is equal '''
+        '''Comparing three profiles, testing when each are the min and when\
+         one taxa is equal '''
         result = compare_profiles(self.many_profiles)
         exresult = {'taxa1': 0.10, 'taxa2': 0.22, 'taxa3': 0.15, 'taxa4': 0.25, 
             'not_shared': 0.28}
@@ -55,11 +61,13 @@ class ProfileTests(TestCase):
             self.assertAlmostEquals(result[key], exresult[key])
 
     def test_compare_profiles_fail(self):
-        '''Make sure error raised when given an empty list or a single profile'''
+        '''Make sure error raised when given an empty list or a single\
+         profile'''
         #empty list check
         self.assertRaises(ValueError, compare_profiles, [])
         #single profile check
-        self.assertRaises(ValueError, compare_profiles, [{'taxa1': 0.20, 'taxa2': 0.30}])
+        self.assertRaises(ValueError, compare_profiles,
+            [{'taxa1': 0.20, 'taxa2': 0.30}])
 
     def test_normalize_profiles_fail(self):
         '''Make sure error raised when given an empty list or single profile'''
@@ -72,20 +80,26 @@ class ProfileTests(TestCase):
     def test_normalize_profiles_eq_len(self):
         '''Normalizing two profiles of equal length'''
         self.assertEquals(normalize_profiles(self.many_profiles),
-            [{'taxa1': 0.20, 'taxa2': 0.30, 'taxa3': 0.15, 'taxa4': 0.35, 'not_shared': 0.00},
-            {'taxa1': 0.10, 'taxa2': 0.50, 'taxa3': 0.15, 'taxa4': 0.25, 'not_shared': 0.00},
-            {'taxa1': 0.15, 'taxa2': 0.22, 'taxa3': 0.15, 'taxa4': 0.48, 'not_shared': 0.00}])
+            [{'taxa1': 0.20, 'taxa2': 0.30, 'taxa3': 0.15, 'taxa4': 0.35,
+             'not_shared': 0.00},
+            {'taxa1': 0.10, 'taxa2': 0.50, 'taxa3': 0.15, 'taxa4': 0.25,
+             'not_shared': 0.00},
+            {'taxa1': 0.15, 'taxa2': 0.22, 'taxa3': 0.15, 'taxa4': 0.48,
+             'not_shared': 0.00}])
 
     def test_normalize_profiles_neq_len(self):
         '''Normalizing two profiles with many diferent taxa'''
         self.assertEquals(normalize_profiles(self.uneven_profiles),
-            [{'taxa1': 0.20, 'taxa2': 0.00, 'taxa3': 0.30, 'taxa4': 0.00, 'taxa5': 0.15, 'taxa6': 0.35, 'taxa7': 0.00, 'not_shared': 0.00},
-            {'taxa1': 0.10, 'taxa2': 0.50, 'taxa3': 0.00, 'taxa4': 0.15, 'taxa5': 0.05, 'taxa6': 0.00, 'taxa7': 0.20, 'not_shared': 0.00}])
+            [{'taxa1': 0.20, 'taxa2': 0.00, 'taxa3': 0.30, 'taxa4': 0.00,
+             'taxa5': 0.15, 'taxa6': 0.35, 'taxa7': 0.00, 'not_shared': 0.00},
+            {'taxa1': 0.10, 'taxa2': 0.50, 'taxa3': 0.00, 'taxa4': 0.15,
+             'taxa5': 0.05, 'taxa6': 0.00, 'taxa7': 0.20, 'not_shared': 0.00}])
 
     def test_compare_profiles_not_shared(self):
         '''Normalizing with one profile containing not_shared and one without'''
         result = compare_profiles(self.not_shared_profiles)
-        exresult = {'taxa1': 0.10, 'taxa2': 0.30, 'taxa3': 0.15, 'taxa4': 0.25, 'not_shared': 0.20}
+        exresult = {'taxa1': 0.10, 'taxa2': 0.30, 'taxa3': 0.15, 'taxa4': 0.25,
+         'not_shared': 0.20}
         #make sure the keys in each are the same
         self.assertEquals(set(result.keys()), set(exresult.keys()))
         #make sure the values are correct-ish
